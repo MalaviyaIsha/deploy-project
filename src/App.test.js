ed8b151636384da442da0af2187aac3caa2ce840
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-07-10 14:09:57 +0530

    remove space from test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Learn Deployment with github pages /i);
+  const linkElement = screen.getByText(/Learn Deployment with github pages/i);
   expect(linkElement).toBeInTheDocument();
 });
